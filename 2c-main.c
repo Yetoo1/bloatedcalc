a24a7909ba426e858a63a204de0383140b1e8fe1
yetoo-server <yetoohappy@gmail.com>
2017-01-05 15:45:36 -0500

    added the proper way its supposed to be not finished

--- 2c-main.c
+++ 2c-main.c
@@ -7,26 +7,31 @@ Compile with: gcc 2c-main.c -o 2c-main.c
 #include <stdio.h>
 #include <string.h>
 #define ARRAY_SIZE 11
-static void math(char* a)
-{
-	int switch;
-	printf("%s\n", a);
-	//insert the case here
-	switch(switch)
-	{
-		case 0: break; //addition
-		case 1: break; //subtraction
-		case 2: break; //multiplication
-		case 3: //division
-	}
-	//ask if user wants to continue or not
-}
 int main (int argc, char** argv)
 {
-	char ask[ARRAY_SIZE];
-	scanf("%s", ask);
+	//char ask[2];
+	int ask[2];
+	char aswitch; //aswitch means ask switch
+	int i;
+	int y;
+	for (i = 0; i < 4; i++)
+	{
+		switch(i)
+		{
+			case 0: break;
+			case 1: printf("+\n"); break;
+			case 2: printf("-\n"); break;
+			case 3: printf("x\n"); break;
+			case 4: printf("/\n"); break;
+			default: printf("\n");	
+		}
+		for (y = 0; y < 9; y++)
+		{
+			printf("%d", y); //fix this so that it returns every three or some shit	
+		}
+	}
+	scanf("Enter a character on the display above: %s", ask);
 	printf("You entered: %s\n", ask);
-	math(ask);
 	return 0;
 
 }
